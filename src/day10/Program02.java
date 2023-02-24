13bee76e80c36b313628b100a6f7c479ed706ff2
kumaravel19980 <125426150+kumaravel19980@users.noreply.github.com>
2023-02-24 16:12:48 +0530

    day11

--- src/day10/Program02.java
+++ src/day10/Program02.java
@@ -1,39 +1,58 @@
 package day10;
 
-import java.util.Scanner;
-
 public class Program02 {
+	//Factorial number
+	// example: 1*2*3*4*5..........n
 
-	// printing tables using while , do while , for statements
+	public static void main(String[] args) {
 
-		public static void main(String[] args) {
-			Scanner a=new Scanner(System.in);
-			System.out.println("enter the table ");
 
-		 int table1=a.nextInt();
-		 a.close();
-
-			int a1=1;
-			while(a1<=30)
-				{
-				System.out.println(table1+"x"+a1+"="+table1*a1);
-				a1++;
+		/*	whileLoop();
+			forLoop();
+			doLoop();
+		}
+		
+		public static void whileLoop()
+		{*/
+			int fact=20;
+			int x=1;
+			long temp=1;
+			while(x<=fact)
+			{
+				temp=temp*x;			
+				x++;
 			}
-			int table2=60;
-			int a2=1;
+			System.out.println(fact+"   "+temp);
+
+
+
+		/*public static void doLoop()
+		{*/{
+			int fact1=8;
+			int x1=1;
+			int temp1=1;
 			do
 			{
-			System.out.println(table2+"x"+a2+"="+table2*a2);
-			a2++;
-		}while(a2<=30);
+				temp1=temp1*x1;			
+				x1++;
+			}while(x1<=fact1);
+			System.out.println(fact1+"   "+temp1);
 
+		}
 
-			int table3=70;
+	/*public static void forLoop()
+		{*/
+		{
+			int fact3=8;
+			int temp3=1;
 
-			for(int a3=1;a3<=30;a3++)
+			for(int x3=1;x<=fact3;x3++)
 			{
-			System.out.println(table3+"x"+a3+"="+table3*a3);
+				temp3=temp3*x3;			
+			}
+			System.out.println(fact3+"   "+temp3);
+		}
 
-		}
-		}
+
+	}
 }
